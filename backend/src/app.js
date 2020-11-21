771ce9eaf11c008e7d8e7c4dc2591922475f1001
ialexanderbrito <xanderzinho26@gmail.com>
2020-11-21 00:39:06 -0300

    👽 Adicionado App e Server

---
+++ backend/src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const routes = require('./routes');
+
+mongoose.connect(
+  'mongodb+srv://alexander:alexander@menuria.ggeef.mongodb.net/menuria?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
+
+const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
+
+app.use((request, response, next) => {
+  request.io = io;
+  return next();
+});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+module.exports = app;
