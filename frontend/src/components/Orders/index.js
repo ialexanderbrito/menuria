6b2d56ded735edb77c941f3123c8ba123f39a719
ialexanderbrito <xanderzinho26@gmail.com>
2020-11-21 17:01:50 -0300

    ✨ Adicionado Card

--- frontend/src/components/Orders/index.js
+++ frontend/src/components/Orders/index.js
@@ -1,5 +1,39 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useState, useEffect } from 'react';
+
+import api from '../../services/api';
+
+import { Container, Card } from './styles';
 
 export default function Orders() {
-  return <h1>Orders!</h1>;
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    async function LoadMenus() {
+      const response = await api.get('/orders');
+
+      setOrders(response.data);
+    }
+    LoadMenus();
+  }, []);
+  return (
+    <Container>
+      {orders.map((order) => (
+        <Card key={order._id} status={order.status}>
+          <header>
+            <h3>
+              Pedido <strong>#{order._id.substr(0, 10)}</strong>
+            </h3>
+            <small>Mesa #{order.table}</small>
+          </header>
+          <p>{order.description}</p>
+          <select value={order.status}>
+            <option value="PENDING">Pendente</option>
+            <option value="PREPARING">Preparando</option>
+            <option value="DONE">Finalizado</option>
+          </select>
+        </Card>
+      ))}
+    </Container>
+  );
 }
