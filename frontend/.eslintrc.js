a41a1bb4c9665ed0b22ffbad41d1894b7dd71c34
ialexanderbrito <xanderzinho26@gmail.com>
2020-11-23 22:14:07 -0300

    🐛 Fix a bug

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: [
-    'airbnb',
-    'prettier',
-    'prettier/react'
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -20,19 +16,14 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier'
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
-      { extensions: ['.jsx', '.js'] }
-    ],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
-    'no-console': ["error", { allow: ["tron"] }],
-    semi: "off"
+    'no-underscore-dangle': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    semi: 'off',
   },
 };
